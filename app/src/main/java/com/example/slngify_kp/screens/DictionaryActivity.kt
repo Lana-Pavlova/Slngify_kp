092701f5e3b7db05d29461549190bd5f0233aa1e
Lana <lana.pavlova2005@yandex.ru>
2024-12-05 01:02:25 +0300

    read words from firestore

--- app/src/main/java/com/example/slngify_kp/screens/DictionaryActivity.kt
+++ app/src/main/java/com/example/slngify_kp/screens/DictionaryActivity.kt
@@ -12,8 +12,62 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.slngify_kp.R
 import androidx.navigation.compose.rememberNavController
+import com.example.slngify_kp.R
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+import com.example.slngify_kp.ui.theme.MyTheme
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material3.TextButton
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import kotlinx.coroutines.tasks.await
+
+
+
 
 class DictionaryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +86,23 @@ class DictionaryActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DictionaryScreen(navController: NavController) {
+    var query by remember { mutableStateOf("") }
+    var wordTranslations by remember { mutableStateOf<List<WordTranslation>>(emptyList()) }
+    var isLoading by remember { mutableStateOf(true) }
+    var error by remember { mutableStateOf<String?>(null) }
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        isLoading = true
+        try {
+            wordTranslations = fetchWordTranslations()
+        } catch (e: Exception) {
+            error = e.message ?: "An unexpected error occurred."
+        } finally {
+            isLoading = false
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -51,22 +122,6 @@ fun DictionaryScreen(navController: NavController) {
             )
         },
         content = { paddingValues ->
-            var query by remember { mutableStateOf("") }
-            val dictionary = listOf(
-                "Привет" to "Hello",
-                "Мир" to "World",
-                "Книга" to "Book",
-                "Кошка" to "Cat",
-                "Собака" to "Dog",
-                "Сленг" to "Slang",
-                "Компьютер" to "Computer",
-                "Телефон" to "Phone"
-            )
-
-            val filteredDictionary = dictionary.filter {
-                it.first.contains(query, ignoreCase = true) || it.second.contains(query, ignoreCase = true)
-            }
-
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -83,26 +138,48 @@ fun DictionaryScreen(navController: NavController) {
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-                LazyColumn {
-                    items(filteredDictionary) { item ->
-                        DictionaryItem(wordPair = item)
-                        Divider()
+                if (isLoading) {
+                    CircularProgressIndicator()
+                } else if (error != null) {
+                    Text("Error: $error")
+                } else {
+                    val filteredWordTranslations = wordTranslations.filter {
+                        it.word.contains(query, ignoreCase = true) || it.translation.contains(query, ignoreCase = true)
+                    }
+                    LazyColumn {
+                        items(filteredWordTranslations) { item ->
+                            DictionaryItem(wordPair = item)
+                            Divider()
+                        }
                     }
                 }
             }
         }
     )
 }
+data class WordTranslation(val word: String, val translation: String)
 
+suspend fun fetchWordTranslations(): List<WordTranslation> {
+    return try {
+        val querySnapshot = Firebase.firestore.collection("wordOfTheDay").get().await()
+        querySnapshot.documents.mapNotNull { document ->
+            val wordOfTheDay = document.toObject(WordOfTheDay::class.java)
+            wordOfTheDay?.let { WordTranslation(it.word, it.translation) }
+        }
+    } catch (e: Exception) {
+        println("Error fetching word translations: ${e.message}")
+        emptyList()
+    }
+}
 @Composable
-fun DictionaryItem(wordPair: Pair<String, String>) {
+fun DictionaryItem(wordPair: WordTranslation) { // Changed to accept WordTranslation
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text(text = wordPair.first, style = MaterialTheme.typography.bodyLarge)
-        Text(text = wordPair.second, style = MaterialTheme.typography.bodyLarge)
+        Text(text = wordPair.word, style = MaterialTheme.typography.bodyLarge)
+        Text(text = wordPair.translation, style = MaterialTheme.typography.bodyLarge)
     }
 }
